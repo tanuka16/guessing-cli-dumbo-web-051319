339b17b7a3a98fa872ab7928ae3ee56c5585b42f
Tanuka Das <das.tanuka@yahoo.com>
2019-05-12 21:55:55 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,8 @@
 # Code your solution here!
 #Taking in input from the command line - Comparing that input to a random number that has been generated
 def run_guessing_game
-
+  loop do
+    puts "Guess a number between 1 to 6"
+    
+  end
 end
