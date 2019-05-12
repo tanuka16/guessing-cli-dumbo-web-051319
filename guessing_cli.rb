955a783e489300ae95c6765c1d5a28f1a700d2c3
Tanuka Das <das.tanuka@yahoo.com>
2019-05-12 22:06:39 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,11 +5,14 @@ def run_guessing_game
     puts "Guess a number between 1 to 6"
     input == gets.chomp
     #generate random number
-    random = rand(1..6)
+    random_num = rand(1..6)
     if input == "exit"
       puts "Goodbye"
-    elsif input.to_i == random
+    elsif input.to_i == random_num
       puts "You guessed the correct number!"
+    elsif input.to_i != random_num
+      puts "The computer guessed #{random_num}"
+
 
   end
 end
