d3dceb0005e5226d8604a4e106dfcfb83f0d2a55
Tanuka Das <das.tanuka@yahoo.com>
2019-05-12 21:57:50 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,9 @@
 def run_guessing_game
   loop do
     puts "Guess a number between 1 to 6"
-    
+    input == gets.chomp
+    #generate random number
+    random = rand(1..6)
+    if random == input
   end
 end
